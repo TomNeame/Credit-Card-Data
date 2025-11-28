4f2a01d9bfa913fad51399034d8c7a16e2948431
NoahBateman <nb626@kent.ac.uk>
2025-11-28 13:49:08 +0000

    Removing repository test. Starting intital data checking.

--- Credit Card Project.R
+++ Credit Card Project.R
@@ -1,6 +1,14 @@
-library(tidyverse)
-test
-test#
-s
 
-#test
+
+dat <- read.csv("BankChurners.csv")
+dat <- dat[, -22:-23] # removing junk columns as instructed on source website
+
+#### Quality Checking ####
+
+# visualising variables
+head(dat) # 'Attrition_Flag' is the outcome showing whether the customer is existing or has left (Attrited)
+
+colSums(is.na(dat)) # no NA values to clean
+
+str(dat) # matches what is expected, may be worth turning 'Attrition_Flag' Binary
+summary(dat) # useful for the numerical columns
